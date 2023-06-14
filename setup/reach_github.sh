687dccfee625d31b37a39c7ab47648b522d5ddcc
miner <xuxin0509@gmail.com>
2023-06-14 13:03:28 +0000

    [setup] fix no curl command issue when running reach_github.sh script

--- setup/reach_github.sh
+++ setup/reach_github.sh
@@ -2,6 +2,12 @@
 
 function reach_github()
 {
+  local curl_path=$(command -v "curl")
+  if [ "x${curl_path}" == "x" ]; then
+    $SUDO ${INSTALL_CMD} curl
+    echo "$(basename "$0"): install curl"
+  fi
+
   local url="https://github.com"
   local code=`curl -I -s ${url} -w %{http_code} | tail -n1`
   if [ "x${code}" == "x200" ]; then

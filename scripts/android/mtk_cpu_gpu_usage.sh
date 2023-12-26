58ccd88b7289ac131a7218d060edd30fe3c28e95
littleminer <nocent@126.com>
2023-12-26 15:47:04 +0800

    get mtk cpu frequency information

--- scripts/android/mtk_cpu_gpu_usage.sh
+++ scripts/android/mtk_cpu_gpu_usage.sh
@@ -5,6 +5,10 @@ if [ $# -ne 1 ]; then
     exit 0
 fi
 
+cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq
+cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq
+cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq
+
 top -o PID,RES,%MEM,%CPU,CMDLINE -s 3 -n 1 | grep -iE "%MEM|$1"
 
 echo -e "\nGPU idle:"

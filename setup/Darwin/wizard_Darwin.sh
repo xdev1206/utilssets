19b610beae2e7b4d16846cd04ede57e00984aeca
Xin Xu <nocent@126.com>
2023-12-31 22:32:40 +0800

    remove ctags based on specific platform that doesn't compatible with mac

--- setup/Darwin/wizard_Darwin.sh
+++ setup/Darwin/wizard_Darwin.sh
@@ -3,17 +3,16 @@
 install_package()
 {
   # necessary package
-  $SUDO ${INSTALL_CMD} curl wget tree
+  $SUDO ${INSTALL_CMD} curl wget tree ctags
 }
 
 shell_env()
 {
   ps1_found=$(cat ${BASH_RC} | grep -c "PS1")
   if [ ${ps1_found} -eq 0 ]; then
-    #export PS1="%F{yellow}%n@%m %d%#%f " for zsh
-    #echo 'export PS1="%F{yellow}%n@%m %d%#%f "' >> ${BASH_RC} for zsh
     export CLICOLOR=1
     export LSCOLORS=ExGxFxdaCxDaDahbadech
+    export HOMEBREW_NO_AUTO_UPDATE=1
     export PS1="\[\e]0;\u@\h: \w\a\]\[\e[33m\]\u@\h:\w\$\[\e[0m\] "
     echo 'export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "' >> ${BASH_RC}
   fi

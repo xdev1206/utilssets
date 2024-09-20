4f8be1091c0e31ab65af0695ba9129ff07b9bc53
xdev <nocent@126.com>
2024-09-20 17:41:15 +0800

    Create java decompile script setup_apk-decompile_env.sh

---
+++ setup/tools/setup_apk-decompile_env.sh
@@ -0,0 +1,9 @@
+wget -c https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool -O apktool
+wget -c https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.10.0.jar -O apktool.jar
+chmod +x apktook*
+
+#wget -c https://sourceforge.net/projects/dex2jar/files/latest/download/dex2jar-2.0.zip
+wget -c https://github.com/DexPatcher/dex2jar/releases/download/v2.1-20190905-lanchon/dex-tools-2.1-20190905-lanchon.zip
+unzip dex-tools-2.1-20190905-lanchon.zip
+
+wget -c https://github.com/java-decompiler/jd-gui/releases/download/v1.6.6/jd-gui-1.6.6.jar

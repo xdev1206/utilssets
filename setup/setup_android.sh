95646b67d9026b4793ec03ab9817fa63543efaa1
xdev <xdev1206@gmail.com>
2025-04-18 13:27:59 +0800

    setup: Update setup_android.sh

--- setup/setup_android.sh
+++ setup/setup_android.sh
@@ -3,55 +3,62 @@
 SCRIPT_PATH=$(cd `dirname $BASH_SOURCE[0]` && /bin/pwd)
 source $SCRIPT_PATH/env/env.sh
 
+if [ "${NETWORK}x" == "0x" ]; then
+    printf "%s\n" "no network connection, exit..."
+    exit 0
+fi
+
 ANDROID_ENV_PATH=${ENV_PATH}/android
-
-cmdlinetools_mac_url='https://dl.google.com/android/repository/commandlinetools-mac-8512546_latest.zip'
-cmdlinetools_linux_url='https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip'
-
-platformtools_linux_url='https://dl.google.com/android/repository/platform-tools-latest-linux.zip'
-
-cmdline_tools_zip=${cmdlinetools_linux_url##*\/}
 sdk_dir=sdk
+
+toolsUrlContents=$(curl https://developer.android.com/studio)
+cmdlinetools_mac_url=$(echo "${toolsUrlContents}" | grep -o "https:\/\/dl.google.com\/android\/repository\/commandlinetools\-mac\-[0-9]*_latest\.zip")
+cmdlinetools_linux_url=$(echo "${toolsUrlContents}" | grep -o "https:\/\/dl.google.com\/android\/repository\/commandlinetools\-linux\-[0-9]*_latest\.zip")
 
 function android_env_dir()
 {
-    rm -rf ${ANDROID_ENV_PATH}
-    mkdir -p ${ANDROID_ENV_PATH}/$sdk_dir
+    if [ ! -d ${ANDROID_ENV_PATH} ]; then
+        mkdir -p ${ANDROID_ENV_PATH}/$sdk_dir
+    fi
 }
 
 function sdk_setup()
 {
-  rm -rf ${cmdline_tools_zip}* > /dev/null
+  ${SUDO} apt update
   ${SUDO} apt install openjdk-17-jdk
+  android_env_dir
+  pushd ${ANDROID_ENV_PATH}/$sdk_dir
 
   if [ "x${OS_TYPE}" == "xDarwin" ]; then
-      cmdline_tools_zip_url=${cmdlinetools_mac_url}
+      cmdlinetools_zip_url=${cmdlinetools_mac_url}
   elif [ "x${OS_TYPE}" == "xLinux" ]; then
-      cmdline_tools_zip_url=${cmdlinetools_linux_url}
+      cmdlinetools_zip_url=${cmdlinetools_linux_url}
   else
-      cmdline_tools_zip_url=${cmdlinetools_linux_url}
+      cmdlinetools_zip_url=${cmdlinetools_linux_url}
   fi
 
-  wget -v $cmdline_tools_zip_url
-  unzip $cmdline_tools_zip -d ${ANDROID_ENV_PATH}/$sdk_dir
-  rm -rf $cmdline_tools_zip
+  cmdlinetools_zip=${cmdlinetools_zip_url##*\/}
+  printf "%s\n" "cmnlinetools_zip: ${cmdlinetools_zip}"
 
-  pushd ${ANDROID_ENV_PATH}/$sdk_dir
+  if [ ! -f ${cmdlinetools_zip} ]; then
+    wget -v $cmdlinetools_zip_url
+    unzip -o $cmdlinetools_zip -d ${ANDROID_ENV_PATH}/$sdk_dir
+  fi
 
   ./cmdline-tools/bin/sdkmanager --sdk_root=. "cmake;3.22.1"
   ./cmdline-tools/bin/sdkmanager --sdk_root=. "build-tools;34.0.0"
   ./cmdline-tools/bin/sdkmanager --sdk_root=. "platform-tools"
   ./cmdline-tools/bin/sdkmanager --sdk_root=. "cmdline-tools;latest"
-  #./cmdline-tools/bin/sdkmanager --sdk_root=. "ndk;23.2.8568313"
-  #./cmdline-tools/bin/sdkmanager --sdk_root=. "platforms;android-25"
+  ./cmdline-tools/bin/sdkmanager --sdk_root=. "ndk;23.2.8568313"
+  ./cmdline-tools/bin/sdkmanager --sdk_root=. "platforms;android-25"
 
-  complete_env_path ${ANDROID_ENV_PATH}/$sdk_dir/platform-tools
+  #complete_env_path ${ANDROID_ENV_PATH}/$sdk_dir/platform-tools
+  #complete_env_path ${ANDROID_ENV_PATH}/$sdk_dir/cmdline-tools/bin
   if [ "$?" == "0" ]; then
-      export ANDROID_SDK_ROOT=${ANDROID_ENV_PATH}/$sdk_dir
+      export_env ANDROID_SDK_ROOT ${ANDROID_ENV_PATH}/$sdk_dir
+      export_env ANDROID_HOME ${ANDROID_ENV_PATH}/$sdk_dir
   fi
   popd
 }
 
-# alread downloaded sdk-tools-linux
-android_env_dir
 sdk_setup

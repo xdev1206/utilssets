fc2f00ce75c2d7839b79f4b9c07f26fe0f84f024
xdev <xdev1206@gmail.com>
2025-07-30 19:42:18 +0800

    Update apk_key_analyzer.sh

--- scripts/android/apk_key_analyzer.sh
+++ scripts/android/apk_key_analyzer.sh
@@ -10,18 +10,23 @@ rm -rf tmp
 APK=$1
 
 BUILD_TOOLS_VERSION=34.0.0
-APKSIGNER=${ANDROID_SDK}/build-tools/${BUILD_TOOLS_VERSION}/apksigner
+ANDROID_SDK="/root/workspace/utilssets/env/android/sdk"
+APKSIGNER="${ANDROID_SDK}/build-tools/${BUILD_TOOLS_VERSION}/apksigner"
 
 # support v2
+echo "apksigner verify"
 ${APKSIGNER} verify -v ${APK}
 ${APKSIGNER} verify --print-certs ${APK}
 ${APKSIGNER} verify --print-certs-pem ${APK}
 
 # only v1/v1+v2, doesn't suport v2 version
+echo "keytool -printcert -jarfile"
 keytool -printcert -jarfile ${APK}
-jarsigner -verify -verbose -certs ${APK}
 
-unzip ${APK} -d tmp
+echo "jarsigner -verify -certs"
+jarsigner -verify -certs ${APK}
+
+unzip -q ${APK} -d tmp
 
 pushd tmp
 

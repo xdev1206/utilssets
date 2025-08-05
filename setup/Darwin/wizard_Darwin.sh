56c4f8ab6f10fcd589670dc94d97cdc2624c1195
xdev <xdev1206@gmail.com>
2025-08-05 11:17:32 +0800

    add zsh ps1 config in wizard_Darwin.sh

--- setup/Darwin/wizard_Darwin.sh
+++ setup/Darwin/wizard_Darwin.sh
@@ -13,8 +13,13 @@ shell_env()
     export CLICOLOR=1
     export LSCOLORS=ExGxFxdaCxDaDahbadech
     export HOMEBREW_NO_AUTO_UPDATE=1
-    export PS1="\[\e]0;\u@\h: \w\a\]\[\e[33m\]\u@\h:\w\$\[\e[0m\] "
-    echo 'export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "' >> ${SHELL_RC}
+    if [ "${SHELL}" = "/bin/bash" ]; then
+        export PS1="\[\e]0;\u@\h: \w\a\]\[\e[33m\]\u@\h:\w\$\[\e[0m\] "
+        echo 'export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "' >> ${SHELL_RC}
+    elif [ "${SHELL}" = "/bin/zsh" ]; then
+        export PS1="%F{green}%n@%f:%F{blue}%~%f%# "
+        echo 'export PS1="%F{green}%n@%f:%F{blue}%~%f%# "' >> ${SHELL_RC}
+    fi
   fi
 
   brew_found=$(echo "$PATH" | grep -ic homebrew)

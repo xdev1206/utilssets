57f58da731edc23547af9e7e4a4fbfd12084c743
xdev <xdev1206@gmail.com>
2025-08-28 11:34:11 +0800

    add eol config in update_git.sh

--- setup/update_git.sh
+++ setup/update_git.sh
@@ -20,3 +20,16 @@ git config --global core.editor vim
 git config --global --add safe.directory /workspace
 git config --global http.version HTTP/1.1 # HTTP/2
 git config --global http.sslBackend gnutls
+
+# 配置diff忽略行尾空白
+# git config --global core.whitespace trailing-space,space-before-tab
+# git config --global apply.whitespace fix
+
+# 忽略所有空白字符的变化
+# git config --global core.autocrlf input
+# git config --global diff.ignoreSubmodules dirty
+
+# 禁用行尾处理
+git config --global core.eol native
+git config --global core.autocrlf false
+git config --global core.safecrlf false

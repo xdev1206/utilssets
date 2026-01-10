c9ab403499249062f7cdb40326dc2b2da502cdfb
xdev <xdev1206@gmail.com>
2026-01-10 16:16:50 +0800

    chore: pyenv setup script, change shebang to sh and clean up functions
    Refactor setup_pyenv.sh to use sh instead of bash and remove unused function.

--- setup/setup_pyenv.sh
+++ setup/setup_pyenv.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 SCRIPT_PATH=$(cd `dirname $BASH_SOURCE[0]` && /bin/pwd)
 
@@ -33,37 +33,12 @@ download_pyenv()
     rm -rf $HOME/.local/lib/python2.7/site-packages/virtualenv* 2>&1
 }
 
-replace_pyenv_complete() {
-  local file="${1:-pyenv.bash}"
-  if [[ ! -f $file ]]; then
-    echo "$file does not exist, please confirm..."
-    return 1
-  fi
-
-  sed -i '' 's/complete -F _pyenv pyenv/compdef _pyenv pyenv/g' "$file"
-  echo "repleace \'complete\' with \'compdef\' in ${file}"
-}
-
 pyenv_to_conf()
 {
-    echo "export PYENV_PATH=$PYENV_PATH" > $PYENV_CONF
-    echo -e 'export PATH="$PYENV_PATH/bin:$PATH"\n\n' >> $PYENV_CONF
+    export_env PYENV_PATH $PYENV_PATH
+    complete_env_path $PYENV_PATH/bin
 
     $PYENV_PATH/bin/pyenv init - >> $PYENV_CONF
-
-    if [ "${OS_TYPE}x" = "Darwinx" ]; then
-        replace_pyenv_complete ${PYENV_PATH}/completions/pyenv.bash
-    fi
-
-    cp $PYENV_CONF $HOME/
-
-    # pip source config
-    if [ ! -f $HOME/.pip/pip.conf ]; then
-        mkdir -p $HOME/.pip
-        echo -e "[global]\nindex-url = https://mirrors.aliyun.com/pypi/simple" >> $HOME/.pip/pip.conf
-        echo -e "\nextra-index-url = https://pypi.tuna.tsinghua.edu.cn/simple" >> $HOME/.pip/pip.conf
-        echo -e "\ntimeout = 120" >> $HOME/.pip/pip.conf
-    fi
 }
 
 python_env()
